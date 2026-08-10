0631c5cc1546221f0f2865057899e0c97128dfb6
KHALID ABDIGANI <almursal45@gmail.com>
2026-08-10 15:05:30 +0300

    add 404 response handling

--- BookLibrary.api/Program.cs
+++ BookLibrary.api/Program.cs
@@ -13,16 +13,21 @@ List<LibraryDTO> books= [
     new (3,"Book3","Author3")
 ];
 //viwe books 
-app.MapGet("/books",
-
- () =>{
-    return books;
+app.MapGet("/books",() =>{
+    if(books is []) return Results.NotFound("Empty");
+    return Results.Ok(books);
+    
+    
     }
 );
 
 
 
-app.MapGet("/books/{id}", (int id) => books.Find(book=>book.id==id))
+app.MapGet("/books/{id}", (int id) =>
+{
+    var book = books.Find(book=>book.id==id);
+    return book is null? Results.NotFound("Book not found") : Results.Ok(book);
+} )
         .WithName(GetBookEndpointName);
 
 
@@ -40,18 +45,31 @@ app.MapPost("/books", (PostBook book) =>
 app.MapPut("/books/{id}", (int id, PutBook UpdateBook) =>
 {
     var i = books.FindIndex(book=>book.id==id);
-    books[i]= new LibraryDTO (
+    if (i < 0)return  Results.NotFound("Book wasnt found");
+    
+        books[i]= new LibraryDTO (
         id,
         UpdateBook.Book_name,
         UpdateBook.Auth_name
         
     );
-    return Results.Text("Updated successfully");
+     return Results.Ok(books[i]);
+    
+    
+    
+        
+    
+     
+    
+    
+    // return Results.Text("Updated successfully");
 });
 
 //delete book 
 app.MapDelete("/books/{id}",(int id)=> {
+
     var i = books.Find(book=>book.id==id);
+    if(i is null)return Results.NotFound("Book was not found");
     books.Remove(i);
     return Results.Text("Deleted successfully");
 });
